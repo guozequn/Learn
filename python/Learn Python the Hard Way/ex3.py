3b3be5c3224bacca2d38a49e2a15c21422d3e8df
guozequn <18610490321@126.com>
2017-11-29 19:16:32 +0800

    add ex3.py

---
+++ python/Learn Python the Hard Way/ex3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/29 19:09
+# @Author  : ZeQun
+# @File    : Exercise 3: Numbers and Math
+
+print("I will no count my chickens:")
+
+
+print("Hens", 25 + 30 / 6 )
+print("Roosters", 100 - 25 * 3 % 4)
+
+
+print("Now i will count the eggs:")
+
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+
+print("is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+
+print("what is 3 + 2?", 3 + 2)
+print("waht is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+
+print("How about some more.")
+
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
