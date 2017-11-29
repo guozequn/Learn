2c1d3d455da93d92d4e5b37f50d68739660377c7
guozequn <18610490321@126.com>
2017-11-29 18:48:25 +0800

    add ex2.py

---
+++ python/Learn Python the Hard Way/ex2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/29 18:44
+# @Author  : ZeQun
+# @File    : Exercise 2: Comments and Pound Characters
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+
+print("I could have code like this.")
+
+
+# You can also use a comment to "disable" or commnet out code:
+# print("This won't run.")
+
+print("This will run.")
