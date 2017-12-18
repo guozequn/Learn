7198e902b40c59fd77d86f25da5168246378678e
ZeQun <18610490321@126.com>
2017-12-18 15:57:58 +0800

    add ex13.py

---
+++ python/Learn Python the Hard Way/11-20/ex13.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/18 15:50
+# @Author  : ZeQun
+# @File    : Parameters , Unpacking, Variables
+
+from sys import argv
+#read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your thrid variable is:", third)
