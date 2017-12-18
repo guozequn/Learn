bc763079324e30a21ad4c94849a6696b8142bb0c
ZeQun <18610490321@126.com>
2017-12-18 15:32:30 +0800

    add ex10.py

---
+++ python/Learn Python the Hard Way/1-10/ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/29 18:37
+# @Author  : ZeQun
+# @File    : Exercise 1: A good First Program
+
+print("Hello World!")
+print("Hello Again!")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
