f0927c5529a69a2912078ead5db00933f49207f1
ZeQun <18610490321@126.com>
2017-12-18 15:50:08 +0800

    add ex11.py  ex12.py

---
+++ python/Learn Python the Hard Way/11-20/ex11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/18 15:36
+# @Author  : ZeQun
+# @File    : Asking Questions
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
---
+++ python/Learn Python the Hard Way/11-20/ex12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/18 15:40
+# @Author  : ZeQun
+# @File    : Prompting People
+
+age = input("How old are you? ")
+height = input("how tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you are {age} old, {height} tall and {weight} heavy.")
