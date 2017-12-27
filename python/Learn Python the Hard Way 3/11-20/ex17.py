3b1730102910bb5e0ddc77b5ac76b21070b3c585
ZeQun <18610490321@126.com>
2017-12-27 17:49:24 +0800

    add ex17.py

---
+++ python/Learn Python the Hard Way 3/11-20/ex17.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/27 17:43
+# @Author  : ZeQun
+# @File    : More Files
+
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+
+# we could do these two on one line , how ?
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print(f"The input file is {len(indata) bytes long")
+
+print(f"Does the output file exist? {exists(to_file)}")
+print("Ready, hit RETURN to continue, CTRL-c to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("Alright, all done.")
+
+out_file.close()
+in_file.close()
